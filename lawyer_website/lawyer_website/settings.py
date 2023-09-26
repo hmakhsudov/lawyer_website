47ba2fa416d162ae71953c56bc1fa249213b8dcb
hmakhsudov <mahsudovhabibullo01@gmail.com>
2023-09-27 00:46:58 +0300

    attempt to create superuser

--- lawyer_website/lawyer_website/settings.py
+++ lawyer_website/lawyer_website/settings.py
@@ -79,10 +79,16 @@ WSGI_APPLICATION = 'lawyer_website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '172.18.0.3',  # Set to the PostgreSQL server hostname or IP address
+        'PORT': '5432',       # Default PostgreSQL port
     }
 }
+
+
 
 
 # Password validation
