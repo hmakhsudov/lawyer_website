025a00fca1a2e2307240bdea7da6cd59b20d5d4f
hmakhsudov <mahsudovhabibullo01@gmail.com>
2023-09-27 00:04:11 +0300

    attempt to create superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# Apply migrations and collect static files
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-# echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+# Check if the superuser already exists
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@admin.com', 'admin') if not User.objects.filter(username='admin').exists() else None"
 
+# Start Gunicorn
 gunicorn lawyer_website.wsgi:application --bind 0.0.0.0:8000
