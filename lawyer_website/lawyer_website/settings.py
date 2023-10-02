c8ee0dc658bd659ef3394611ed435d878b6ad189
hmakhsudov <mahsudovhabibullo01@gmail.com>
2023-10-02 21:39:12 +0300

    wrong statics on local plus csrf secure

--- lawyer_website/lawyer_website/settings.py
+++ lawyer_website/lawyer_website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ycopsf32+%$gqa$xgc4yc6s-#k$@9fjvgwah2vi9_ox&-s2ke!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
