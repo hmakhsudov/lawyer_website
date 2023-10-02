17069d70df472a3ad1cb540f06712c0dffc87bbd
hmakhsudov <mahsudovhabibullo01@gmail.com>
2023-10-02 21:20:35 +0300

    wrong statics on local plus csrf secure

--- lawyer_website/lawyer_website/settings.py
+++ lawyer_website/lawyer_website/settings.py
@@ -78,13 +78,17 @@ WSGI_APPLICATION = 'lawyer_website.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'mydb',
+    #     'USER': 'myuser',
+    #     'PASSWORD': 'mypassword',
+    #     'HOST': '172.19.0.2',  # Set to the PostgreSQL server hostname or IP address
+    #     'PORT': '5432',       # Default PostgreSQL port
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER': 'myuser',
-        'PASSWORD': 'mypassword',
-        'HOST': '172.19.0.2',  # Set to the PostgreSQL server hostname or IP address
-        'PORT': '5432',       # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -130,6 +134,8 @@ STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'lawyer_website/static/'),
 ]
+
+CSRF_COOKIE_SECURE = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
