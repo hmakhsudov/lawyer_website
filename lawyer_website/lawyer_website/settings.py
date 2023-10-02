d90775d751ee1c04a57235a3513c72283527a7f3
hmakhsudov <mahsudovhabibullo01@gmail.com>
2023-10-02 22:12:38 +0300

    wrong statics on local plus csrf secure

--- lawyer_website/lawyer_website/settings.py
+++ lawyer_website/lawyer_website/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'lawyer_website.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'mydb',
-    #     'USER': 'myuser',
-    #     'PASSWORD': 'mypassword',
-    #     'HOST': '172.19.0.2',  # Set to the PostgreSQL server hostname or IP address
-    #     'PORT': '5432',       # Default PostgreSQL port
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': '172.19.0.2',  # Set to the PostgreSQL server hostname or IP address
+        'PORT': '5432',       # Default PostgreSQL port
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
