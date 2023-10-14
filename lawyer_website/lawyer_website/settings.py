9c227075faa656e56fdd44a552e07cce92a79007
hmakhsudov <mahsudovhabibullo01@gmail.com>
2023-10-14 15:23:46 +0300

    changed iP

--- lawyer_website/lawyer_website/settings.py
+++ lawyer_website/lawyer_website/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'USER': 'myuser',
         'PASSWORD': 'mypassword',
         'HOST': '172.24.0.2',  # Set to the PostgreSQL server hostname or IP address
-        'PORT': '5432',       # Default PostgreSQL port
+        'PORT': 5432,       # Default PostgreSQL port
     }
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
